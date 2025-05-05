a9d4d76c53c943acc49dc293ef8b320be159e390
Alfredo Quezada <jquezadasoler@gmail.com>
2025-05-05 14:39:45 -0600

    Ya avanazamos, solo queda plasmar las ideas en algo bien estructurado.
    PD: Bienvenido Python

--- compiler/src/AFN.java
+++
@@ -1,9 +0,0 @@
-public class AFN {
-    public Estado inicio;
-    public Estado fin;
-
-    public AFN(Estado inicio, Estado fin) {
-        this.inicio = inicio;
-        this.fin = fin;
-    }
-}
--- compiler/src/RegexToAFD.java
+++
@@ -1,9 +0,0 @@
-public class RegexToAFD {
-
-    public String aumentarRegex(String regex) {
-        String extraValue = "#";
-        regex = regex + extraValue;
-        return regex;
-    }
-    
-}
